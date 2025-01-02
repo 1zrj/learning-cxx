1fa6778f17fb73b5abdee4af2348220b3a8b7117
1zrj <819519226@qq.com>
2025-01-02 22:36:10 +0800

    update

--- exercises/23_template_const/main.cpp
+++ exercises/23_template_const/main.cpp
@@ -11,7 +11,10 @@ struct Tensor {
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
-        for(int i = 0; i < N; i ++) size *= shape[i];
+        for(int i = 0; i < N; i ++) {
+            shape[i] = shape_[i];
+            size *= shape[i];
+        }
         data = new T[size];
         std::memset(data, 0, size * sizeof(T));
     }
@@ -32,16 +35,11 @@ struct Tensor {
 
 private:
     unsigned int data_index(unsigned int const indices[N]) const {
-        unsigned int index = 0;
-        for (unsigned int i = 0; i < N; ++i) {
+        unsigned int index = 0, mul = 1;
+        for (unsigned int i = N - 1; i < N; -- i) {
             ASSERT(indices[i] < shape[i], "Invalid index");
-            // TODO: 计算 index
-            if(!i) index += indices[i];
-            else {
-                index *= indices[i];
-                index += indices[i];
-            }
-
+            index += indices[i] * mul;
+            mul *= shape[i];
         }
         return index;
     }
